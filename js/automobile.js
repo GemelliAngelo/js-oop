4993b3aa6f3d234792bba88bbeffbf139711123a
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-11 14:26:00 +0100

    add new class extend

---
+++ js/automobile.js
@@ -0,0 +1,9 @@
+class Automobile extends Veicolo {
+  numeroPorte;
+  carburante;
+
+  constructor(numeroPorte, carburante) {
+    this.numeroPorte = numeroPorte;
+    this.carburante = carburante;
+  }
+}
