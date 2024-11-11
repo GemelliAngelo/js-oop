35d89aa297fa6c451aa2d6ccf26c4fe5d773eb2d
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-11 14:37:45 +0100

    complete + bonus

--- js/automobile.js
+++ js/automobile.js
@@ -2,7 +2,8 @@ class Automobile extends Veicolo {
   numeroPorte;
   carburante;
 
-  constructor(numeroPorte, carburante) {
+  constructor(marca, anno, colore, numeroPorte, carburante) {
+    super(marca, anno, colore);
     this.numeroPorte = numeroPorte;
     this.carburante = carburante;
   }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 const Fiat = new Veicolo("Fiat", 2019, "blu");
 console.log(Fiat.informazioni());
 console.log(Fiat.calcolaEta());
+
+const Opel = new Automobile("Opel", 2020, "nero", 4, "benzina");
+console.log(
+  `Questa automobile ha ${Opel.numeroPorte} porte ed è alimentata a ${Opel.carburante}`
+);
